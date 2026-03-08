7e3d45690c348c6dab761520efe853a4bbf29086
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-03-08 17:29:31 +0530

    chore(ratelimit): increase max requests from 50 to 500 per window

--- server/src/midlleware/rateLimiter.js
+++ server/src/midlleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit'
 
 const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
-    max: 50,
+    max: 500,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
